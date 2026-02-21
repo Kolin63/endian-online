3f42decd3200436f720a697992f34b4b8d8fe416
Kolin63 <theverylastgoodname@gmail.com>
2026-02-21 01:54:11 -0500

    Update log.h

--- src/log.h
+++ src/log.h
@@ -1,12 +1,19 @@
 #ifndef ENDIAN_LOG_H_
 #define ENDIAN_LOG_H_
 
-#define log_log(level, ...) logmod_log(level, logmod_get_logger(discord_get_logmod(bot_get_global()->discord_bot), "CLIENT"), __VA_ARGS__);
+#define LOGMOD_STATIC
+#include <concord/logmod.h>
+
+#define log_log(level, ...)                                                    \
+  logmod_log(level,                                                            \
+             logmod_get_logger(                                                \
+                 discord_get_logmod(bot_get_global()->discord_bot), "CLIENT"), \
+             __VA_ARGS__);
 
 #define log_trace(...) log_log(TRACE, __VA_ARGS__)
 #define log_debug(...) log_log(DEBUG, __VA_ARGS__)
-#define log_info(...)  log_log(INFO,  __VA_ARGS__)
-#define log_warn(...)  log_log(WARN,  __VA_ARGS__)
+#define log_info(...) log_log(INFO, __VA_ARGS__)
+#define log_warn(...) log_log(WARN, __VA_ARGS__)
 #define log_error(...) log_log(ERROR, __VA_ARGS__)
 #define log_fatal(...) log_log(FATAL, __VA_ARGS__)
 
