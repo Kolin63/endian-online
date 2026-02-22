d34dfe603a805e34cdcc77901589b2afba20cf19
Kolin63 <theverylastgoodname@gmail.com>
2026-02-22 13:39:10 -0500

    Simplify fileio_read_all()

--- src/fileio.c
+++ src/fileio.c
@@ -1,15 +1,13 @@
 #include "fileio.h"
 
+#include <stdlib.h>
 #include <string.h>
 
 void fileio_read_all(struct fileio* buf, FILE* file) {
   char* line = malloc(65536);
   while (fgets(line, sizeof(line), file) != NULL) {
     buf->buf_size += strlen(line);
-    char* new_buf = malloc(buf->buf_size);
-    strcpy(new_buf, buf->buf);
-    free(buf->buf);
-    buf->buf = new_buf;
+    buf->buf = realloc(buf->buf, buf->buf_size);
     strcat(buf->buf, line);
   }
   free(line);
