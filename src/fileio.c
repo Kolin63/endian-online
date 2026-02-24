457999c3071f59409e67228b4d93acf7eb28c94b
Kolin63 <theverylastgoodname@gmail.com>
2026-02-24 16:17:25 -0500

    Fix fileio_read_all() line size

--- src/fileio.c
+++ src/fileio.c
@@ -3,9 +3,11 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define LINE_BUF_SIZE 65536
+
 void fileio_read_all(struct fileio* buf, FILE* file) {
-  char* line = malloc(65536);
-  while (fgets(line, sizeof(line), file) != NULL) {
+  char* line = malloc(LINE_BUF_SIZE);
+  while (fgets(line, LINE_BUF_SIZE, file) != NULL) {
     buf->buf_size += strlen(line);
     buf->buf = realloc(buf->buf, buf->buf_size);
     strcat(buf->buf, line);
