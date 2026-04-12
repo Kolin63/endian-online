13eb1906061ee3ad5af1a0eea4581178d8cc6dcf
Kolin63 <theverylastgoodname@gmail.com>
2026-04-11 23:02:29 -0400

    Improve error messages within JSON checking macros

--- src/json_macros.h
+++ src/json_macros.h
@@ -7,18 +7,18 @@
 // - const char* file_name
 // - const char* mod_name
 // - int error
-#define END_JSON_CHECK_STRING                                               \
-  if (iter->json->type != cJSON_String) {                                   \
-    log_error("In command %s from mod %s, %s must be string", file_name,    \
-              mod_name, iter->json->string);                                \
-    error++;                                                                \
-    continue;                                                               \
-  }                                                                         \
-  if (iter->json->valuestring == NULL) {                                    \
-    log_error("In command %s from mod %s, %s is NULL", file_name, mod_name, \
-              iter->json->string);                                          \
-    error++;                                                                \
-    continue;                                                               \
+#define END_JSON_CHECK_STRING                                                 \
+  if (iter->json->type != cJSON_String) {                                     \
+    log_error("In data type %s from mod %s, %s must be string", file_name,    \
+              mod_name, iter->json->string);                                  \
+    error++;                                                                  \
+    continue;                                                                 \
+  }                                                                           \
+  if (iter->json->valuestring == NULL) {                                      \
+    log_error("In data type %s from mod %s, %s is NULL", file_name, mod_name, \
+              iter->json->string);                                            \
+    error++;                                                                  \
+    continue;                                                                 \
   }
 
 // checks that string length is: min <= length <= max
@@ -27,15 +27,15 @@
 // - const char* file_name
 // - const char* mod_name
 // - int error
-#define END_JSON_CHECK_STRING_LENGTH(min, max)                            \
-  {                                                                       \
-    int len = strlen(iter->json->valuestring);                            \
-    if (len < min || len > max) {                                         \
-      log_error("In command %s from mod %s, %s is wrong size", file_name, \
-                mod_name, iter->json->string);                            \
-      error++;                                                            \
-      continue;                                                           \
-    }                                                                     \
+#define END_JSON_CHECK_STRING_LENGTH(min, max)                              \
+  {                                                                         \
+    int len = strlen(iter->json->valuestring);                              \
+    if (len < min || len > max) {                                           \
+      log_error("In data type %s from mod %s, %s is wrong size", file_name, \
+                mod_name, iter->json->string);                              \
+      error++;                                                              \
+      continue;                                                             \
+    }                                                                       \
   }
 
 // checks that an array is the proper type and has at least one child
@@ -44,18 +44,18 @@
 // - const char* file_name
 // - const char* mod_name
 // - int error
-#define END_JSON_CHECK_ARRAY                                              \
-  if (iter->json->type != cJSON_Array) {                                  \
-    log_error("In command %s from mod %s, %s must be array", file_name,   \
-              mod_name, iter->json->string);                              \
-    error++;                                                              \
-    continue;                                                             \
-  }                                                                       \
-  if (iter->json->child == NULL) {                                        \
-    log_error("In command %s from mod %s, %s has no children", file_name, \
-              mod_name, iter->json->string);                              \
-    error++;                                                              \
-    continue;                                                             \
+#define END_JSON_CHECK_ARRAY                                                \
+  if (iter->json->type != cJSON_Array) {                                    \
+    log_error("In data type %s from mod %s, %s must be array", file_name,   \
+              mod_name, iter->json->string);                                \
+    error++;                                                                \
+    continue;                                                               \
+  }                                                                         \
+  if (iter->json->child == NULL) {                                          \
+    log_error("In data type %s from mod %s, %s has no children", file_name, \
+              mod_name, iter->json->string);                                \
+    error++;                                                                \
+    continue;                                                               \
   }
 
 #endif
